9ca2773ba52fa563ffc605783166bed2ce74bd15
crazyboomegg <39693547+crazyboomegg@users.noreply.github.com>
2020-10-18 04:02:46 +0800

    Add files via upload

---
+++ 1_AdaptiveUIDemo/AdaptiveUIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveUIDemo
+//
+//  Created by 江柏毅 on 2020/10/15.
+//  Copyright © 2020 Boy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
