ed9d1a1b78316446b501dc04a78c081ab4d65c6c
crazyboomegg <crazyboomegg@gmail.com>
2020-10-30 07:37:54 +0800

    no message

---
+++ 12_SimpleCamera/SimpleCamera/SimpleCameraController.swift
@@ -0,0 +1,193 @@
+//
+//  SimpleCameraController.swift
+//  SimpleCamera
+//
+//  Created by Simon Ng on 16/10/2019.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class SimpleCameraController: UIViewController, AVCapturePhotoCaptureDelegate {
+
+    let captureSession = AVCaptureSession()
+    
+    var backFacingCamera: AVCaptureDevice?
+    var frontFacingCamera: AVCaptureDevice?
+    var currentDevice: AVCaptureDevice!
+    
+    var stillImageOutput: AVCapturePhotoOutput!
+    var stillImage: UIImage?
+    
+    var cameraPreviewLayer: AVCaptureVideoPreviewLayer?
+    
+    var toggleCameraGestureRecognizer = UISwipeGestureRecognizer()
+    
+    var zoomInGestureRecognizer = UISwipeGestureRecognizer()
+    var zoomOutGestureRecognizer = UISwipeGestureRecognizer()
+    
+    @IBOutlet var cameraButton:UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       configure()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Action methods
+    
+    @IBAction func capture(sender: UIButton) {
+        
+        let photoSettings = AVCapturePhotoSettings(format: [AVVideoCodecKey: AVVideoCodecType.jpeg])
+        photoSettings.isAutoStillImageStabilizationEnabled = true
+        photoSettings.isHighResolutionPhotoEnabled = true
+        photoSettings.flashMode = .auto
+        
+        stillImageOutput.isHighResolutionCaptureEnabled = true
+        stillImageOutput.capturePhoto(with: photoSettings, delegate: self)
+        
+    }
+
+    // MARK: - Segues
+    
+    @IBAction func unwindToCameraView(segue: UIStoryboardSegue) {
+    
+    }
+    
+    private func configure() {
+        captureSession.sessionPreset = AVCaptureSession.Preset.photo
+        
+        let deviceDiscoverySession = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .unspecified)
+        
+        for device in deviceDiscoverySession.devices {
+            if device.position == .back {
+                backFacingCamera = device
+            } else if device.position == .front {
+                frontFacingCamera = device
+            }
+        }
+        
+        currentDevice = backFacingCamera
+        
+        guard let captureDeviceInput = try? AVCaptureDeviceInput(device: currentDevice) else {
+            return
+        }
+        
+        stillImageOutput = AVCapturePhotoOutput()
+        
+        captureSession.addInput(captureDeviceInput)
+        captureSession.addOutput(stillImageOutput)
+        
+        cameraPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        view.layer.addSublayer(cameraPreviewLayer!)
+        cameraPreviewLayer?.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        cameraPreviewLayer?.frame = view.layer.frame
+        
+        view.bringSubviewToFront(cameraButton)
+        captureSession.startRunning()
+        
+        toggleCameraGestureRecognizer.direction = .up
+        toggleCameraGestureRecognizer.addTarget(self, action: #selector(toggleCamera))
+        view.addGestureRecognizer(toggleCameraGestureRecognizer)
+        
+        zoomInGestureRecognizer.direction = .right
+        zoomInGestureRecognizer.addTarget(self, action: #selector(zoomIn))
+        view.addGestureRecognizer(zoomInGestureRecognizer)
+        
+        zoomOutGestureRecognizer.direction = .left
+        zoomOutGestureRecognizer.addTarget(self, action: #selector(zoomOut))
+        view.addGestureRecognizer(zoomOutGestureRecognizer)
+        
+    }
+    
+    @objc func zoomIn() {
+        if let zoomFactor = currentDevice?.videoZoomFactor {
+            if zoomFactor < 5.0 {
+                let newZoomFactor = min(zoomFactor + 1.0, 5.0)
+                do {
+                    try currentDevice?.lockForConfiguration()
+                    currentDevice?.ramp(toVideoZoomFactor: newZoomFactor, withRate: 1.0)
+                    currentDevice?.unlockForConfiguration()
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    @objc func zoomOut() {
+        if let zoomFactor = currentDevice?.videoZoomFactor {
+            if zoomFactor > 1.0 {
+                let newZoomFactor = max(zoomFactor - 1.0, 1.0)
+                do {
+                    try currentDevice?.lockForConfiguration()
+                    currentDevice?.ramp(toVideoZoomFactor: newZoomFactor, withRate: 1.0)
+                    currentDevice?.unlockForConfiguration()
+                } catch {
+                    print(error)
+                }
+            }
+        }
+    }
+    
+    
+    
+    
+    
+    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+        guard error == nil else {
+            return
+        }
+        
+        guard let imageData = photo.fileDataRepresentation() else {
+            return
+        }
+        
+        stillImage = UIImage(data: imageData)
+        performSegue(withIdentifier: "showPhoto", sender: self)
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showPhoto" {
+            let photoViewController = segue.destination as! PhotoViewController
+            photoViewController.image = stillImage
+        }
+    }
+
+    @objc func toggleCamera() {
+        captureSession.beginConfiguration()
+        
+        guard let newDevice = (currentDevice?.position == AVCaptureDevice.Position.back) ? frontFacingCamera : backFacingCamera else {
+            return
+        }
+        
+        for input in captureSession.inputs {
+            captureSession.removeInput(input as! AVCaptureDeviceInput)
+        }
+        
+        let cameraInput:AVCaptureDeviceInput
+        do {
+            cameraInput = try AVCaptureDeviceInput(device: newDevice)
+        } catch {
+            print(error)
+            return
+        }
+        
+        if captureSession.canAddInput(cameraInput) {
+            captureSession.addInput(cameraInput)
+        }
+        
+        currentDevice = newDevice
+        captureSession.commitConfiguration()
+        
+    }
+    
+    
+}
